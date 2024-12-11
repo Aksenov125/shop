d84b00f6b279145f015c762c88d61dd90925ad4f
Aksenov125 <nik.aksenovv@gmail.com>
2024-12-11 17:57:19 +1000

    debug

--- client/src/features/Catalog/CategoryList.tsx
+++ client/src/features/Catalog/CategoryList.tsx
@@ -3,23 +3,30 @@ import { useSelector } from "react-redux"
 import { RootState, useAppDispatch } from "../../store/store"
 import CategoriesItem from "./CategoryItem"
 import { categoriesThunk, removeCategory } from "../Main/mainPageSlice";
+import ModalWindow from "../Main/components/ModalWindow";
+import { useState } from "react";
+
 
 function CategoriesList(): JSX.Element{
     const categories = useSelector((store:RootState) => store.categories.categories)
     const dispatch = useAppDispatch();
-    const removePreferencess = async (id: number) => {
+    const toggleModal = () => setIsModalOpen(prev => !prev);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const removeCategories = async (id: number) => {
         await dispatch(removeCategory(id)).catch(console.log);
         dispatch(categoriesThunk()).catch(console.log);
       };
     return (
         <div>
+              <button onClick={toggleModal}>Открыть модальное окно</button>
+              <ModalWindow isOpen={isModalOpen} onClose={setIsModalOpen} />
             {categories.map((category) => (
                 <div>
                 <CategoriesItem  key={category.id}  category={category}/>
                 <button
                 className="btn__del"
                 type="button"
-                onClick={() => removePreferencess(category.id)}
+                onClick={() => removeCategories(category.id)}
               >
                 удалить
               </button>
